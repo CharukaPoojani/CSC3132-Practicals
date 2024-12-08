d766043d6f7b890f5d8c336c2687afb276f05251
CharukaPoojani <99280781+CharukaPoojani@users.noreply.github.com>
2024-12-08 15:19:49 +0000

    my function file updated

--- myfunc.php
+++ myfunc.php
@@ -41,7 +41,52 @@ function print_table($name,$connect,$arr){
         die($e->getmessage());
     }
     }
+    function get_student($connect){
+        //query
+        try{
+        
+            $sql="select id,name,age from students  ";
+            
+        
+            //$sq1="SELECT * FROM $name";
+            
+        
+            //execute the query
+            $result=mysqli_query($connect,$sql);
+        
+            //check if data exists in the table
+            if(mysqli_num_rows($result)>0){
+                echo "<table border='1'>";
+                $col=mysqli_fetch_fields($result);
+                echo "<tr>";
+                foreach($col as $value){
+                    echo "<td>$value->name</td>";
+                }
+                echo "<td>Marks</td>";
+                echo "</tr>";
+                
 
+               
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr>";
+                    foreach($row as $key=>$value){
+                        echo "<td>$value</td>";
+                    }
+                    $id=$row['id'];
+                    echo "<td><a href='StudentDetails.php?id=$id'>View</a></td>";
+                    echo "</tr>";
+                   
+                }
+                echo "</table>";
+            }else{
+                echo "No results";
+            }
+        }
+        catch(Exception $e){
+            die($e->getmessage());
+        }
+        }
+    
     function search_student($name,$connect){
         try{
     
@@ -55,10 +100,10 @@ function print_table($name,$connect,$arr){
                 echo "<table border='1'>";
                 $col=mysqli_fetch_fields($result);
                 echo "<tr>";
-                /*foreach($arr as $col){
-                    echo "<td>$col</td>";
+                foreach($col as $value){
+                    echo "<td>$value->name</td>";
                 }
-                echo "</tr>";*/
+                echo "</tr>";
                
                 while($row = mysqli_fetch_assoc($result)){
                     echo "<tr>";
@@ -76,7 +121,42 @@ function print_table($name,$connect,$arr){
             die($e->getmessage());
         }
     }
-
+    function getstudentdetails($id,$connect){
+        try{
+    
+            $sql="select * from students where id='$id' ";
+            
+           
+            $result=mysqli_query($connect,$sql);
+        
+            //check if data exists in the table
+            if(mysqli_num_rows($result)>0){
+                echo "<table border='1'>";
+                $col=mysqli_fetch_fields($result);
+                echo "<tr>";
+                foreach($col as $value){
+                    echo "<td>$value->name</td>";
+                }
+                echo "</tr>";
+               
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr>";
+                    foreach($row as $key=>$value){
+                        echo "<td>$value</td>";
+                    }
+                    $id=$row['id'];
+                    echo "<td><a href='StudentDetails.php?id=$id'>View</a></td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }else{
+                echo "No results";
+            }
+        }
+        catch(Exception $e){
+            die($e->getmessage());
+        }
+    }
     
 
     ?>
